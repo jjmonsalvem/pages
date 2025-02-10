c1cfdb20c67673c6a86231bae8792ed86f93c46c
jjmonsalvem <jjmonsalvem@unal.edu.co>
2025-02-09 21:37:25 -0500

    Se realiza actualizacion del Blog implementando un CRUD

--- blog/tests.py
+++ blog/tests.py
@@ -1,48 +1,33 @@
-from django.contrib.auth import get_user_model
-from django.test import TestCase
+# blog/tests.py
 from django.urls import reverse
+from blog.models import Post
 
-from .models import Post
 
-# Create your tests here.
-class BlogTests(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = get_user_model().objects.create_user(
-            username="testuser", email="test@email.com", password="secret"
-        )
+def test_post_createview(self):  # nuevo
+    response = self.client.post(
+        reverse("post_new"),
+        {
+            "title": "Nuevo título",
+            "body": "Nuevo texto",
+            "author": self.user.id,
+        },
+    )
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual(Post.objects.last().title, "Nuevo título")
+    self.assertEqual(Post.objects.last().body, "Nuevo texto")
 
-        cls.post = Post.objects.create(
-            title="Un buen título", body="Contenido del cuerpo agradable", author=cls.user,
-        )
+def test_post_updateview(self):  # nuevo
+    response = self.client.post(
+        reverse("post_edit", args="1"),
+        {
+            "title": "Título actualizado",
+            "body": "Texto actualizado",
+        },
+    )
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual(Post.objects.last().title, "Título actualizado")
+    self.assertEqual(Post.objects.last().body, "Texto actualizado")
 
-    def test_post_model(self):
-        self.assertEqual(self.post.title, "Un buen título")
-        self.assertEqual(self.post.body, "Contenido del cuerpo agradable")
-        self.assertEqual(self.post.author.username, "testuser")
-        self.assertEqual(str(self.post), "Un buen título")
-        self.assertEqual(self.post.get_absolute_url(), "/post/1/")
-
-    def test_url_exists_at_correct_location_listview(self):  # nuevo
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
-
-    def test_url_exists_at_correct_location_detailview(self):  # nuevo
-        response = self.client.get("/post/1/")
-        self.assertEqual(response.status_code, 200)
-
-    def test_post_listview(self):  # nuevo
-        response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Contenido del cuerpo agradable")
-        self.assertTemplateUsed(response, "home.html")
-
-    def test_post_detailview(self):  # nuevo
-        response = self.client.get(reverse("post_detail",
-          kwargs={"pk": self.post.pk}))
-        no_response = self.client.get("/post/100000/")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(no_response.status_code, 404)
-        self.assertContains(response, "Un buen título")
-        self.assertTemplateUsed(response, "post_detail.html")
-
+def test_post_deleteview(self):  # nuevo
+    response = self.client.post(reverse("post_delete", args="1"))
+    self.assertEqual(response.status_code, 302)
